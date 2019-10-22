535cd323662fc83da38bcf2d1200576db0789184
Kyle <kylepw@gmail.com>
2019-10-22 12:13:02 +0900

    complete ch4 ticket 1

--- mflix-python/mflix/db.py
+++ mflix-python/mflix/db.py
@@ -47,8 +47,11 @@ def get_db():
         MFLIX_DB_URI,
         # TODO: Connection Pooling
         # Set the maximum connection pool size to 50 active connections.
+        maxPoolSize=50,
         # TODO: Timeouts
         # Set the write timeout limit to 2500 milliseconds.
+        w="majority",
+        wTimeoutMS=2500,
         )[MFLIX_DB_NAME]
     return db
 
